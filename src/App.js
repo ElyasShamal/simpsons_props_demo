0732df5e83c2820a782ae1325a3f404b9111abec
Elyas <Elyas.shamal2@gmail.com>
2023-06-24 22:45:40 -0400

    render all data from data.js

--- src/App.js
+++ src/App.js
@@ -7,11 +7,14 @@ function App() {
     <div className="App">
       <h1 className="main-title">The Simpsons</h1>
       <div className="container">
-        <Character
-          name={characters[0].name}
-          image={characters[0].image}
-          quote={characters[0].quotes}
-        />
+        <Character character={characters[0]} />
+        <Character character={characters[1]} />
+        <Character character={characters[2]} />
+        <Character character={characters[3]} />
+        <Character character={characters[4]} />
+        <Character character={characters[5]} />
+        <Character character={characters[6]} />
+        <Character character={characters[7]} />
       </div>
     </div>
   );
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Quote from "./Quote";
 
-function Character({ name, image, quote }) {
+function Character({ character }) {
+  const { name, image, quotes } = character;
   return (
     <div className="card">
       <img src={image} alt={name} />
       <h2>{name}</h2>
-      <Quote quote={quote[0]} />
-      <Quote quote={quote[1]} />
-      <Quote quote={quote[2]} />
+      <Quote quote={quotes[0]} />
+      <Quote quote={quotes[1]} />
+      <Quote quote={quotes[2]} />
     </div>
   );
 }
